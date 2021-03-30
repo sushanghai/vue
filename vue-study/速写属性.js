49249b25f7ec80ef5d5c7f43d2f30ab61405f187
Tiger <sushanghai@qq.com>
2021-03-30 08:24:10 +0800

    v20210330

---
+++ vue-study/速写属性.js
@@ -0,0 +1,8 @@
+var name = "成哥";
+var age = 18;
+var  person ={
+    name,
+    age,
+};
+
+console.log(person);
---
+++ vue-study/速写方法.js
@@ -0,0 +1,9 @@
+var  person ={
+    name:"成哥",
+    age: 18,
+    sayhello() {  //省略一个": funtion()"
+        console.log("你好！");
+},
+};
+
+person.sayhello()
